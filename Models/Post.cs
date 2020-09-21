57c1827015a8a7713951cfea84a58fa4f7a893c2
P1atyPe <p1atype50446@gmail.com>
2020-09-21 23:02:37 +0300

    - Created Database - Added Migration "init"

--- Models/Post.cs
+++ Models/Post.cs
@@ -4,6 +4,8 @@ namespace MVCShop.Models
 {
     public class Post
     {
+        public int Id { get; set; }
+
         public string Title { get; set; } = "";
         public string Body { get; set; } = "";
 
--- Startup.cs
+++ Startup.cs
@@ -5,15 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVCShop.Data;
 
 namespace MVCShop
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
+
             services.AddControllersWithViews();
         }
 
