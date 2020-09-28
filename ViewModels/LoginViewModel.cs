98c3c2ffab6d120bea11f96bf283eefd7f80b650
P1atyPe <p1atype50446@gmail.com>
2020-09-28 16:49:04 +0300

    - User Sign In

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MVCShop.ViewModels
 {
     public class LoginViewModel
     {
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
